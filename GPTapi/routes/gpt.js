19b3a5201a7a3a07d64d7cc94dacc9d94c01e223
jinhyeong.ju <jujh97@hanmail.net>
2023-11-16 01:20:51 +0900

    move comment

--- GPTapi/routes/gpt.js
+++ GPTapi/routes/gpt.js
@@ -24,12 +24,14 @@ router.post('/modify_scene', (req, res) => __awaiter(void 0, void 0, void 0, fun
         const completion = yield openai.chat.completions.create({
             messages: [
                 { "role": "system", "content": "you are a good scene modifier. Use the provided function to answer questions." },
+                { "role": "system", "content": "you need to reply with a description of result" },
                 { "role": "user", "content": content },
+                // "Move the position of nano by 10 in the x direction, rotate 90 degrees from the y-axis, scaling twice as big in the z direction"
             ],
             "model": "gpt-3.5-turbo-1106",
             "tools": (0, gptService_1.makeTools)(nameArr),
         });
-        // "Move the position of nano by 10 in the x direction, rotate 90 degrees from the y-axis, scaling twice as big in the z direction"
+        // change scene... using tools
         console.log(JSON.stringify(completion.choices[0]));
         res.send(completion.choices[0]);
     }
